1b7f6ba86e934d485beb53577aac7182e09eabda
Timothy Stanley <79507886+Stanley77-web@users.noreply.github.com>
2022-01-26 11:39:00 +0700

    Delete Tucil-1-Stima directory

--- Tucil-1-Stima/src/Helper.java
+++
@@ -1,9 +0,0 @@
-public class Helper {
-    public static String coloring(int n) { 
-        if (n == -1) {
-            return "\033[0m";
-        } else {
-            return "\033[38;5;" + Integer.toString(n) + "m";
-        } 
-    } 
-}
